7699cc52db1b2b60169f083fab30b605d804b696
Климанский Матвей <ak1986tutu@gmail.com>
2026-03-21 02:02:00 +0800

    0.4 feat: add start func, add route connect

---
+++ app/main.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastapi import FastAPI
+from api.routes import router as shortener_router
+
+
+app = FastAPI(
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None,
+)
+
+# Routes connect
+app.include_router(shortener_router)
+
+async def run():
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000
+        #reload=True
+    )
+
+if __name__ == "__main__":
+    run()
